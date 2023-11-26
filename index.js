7ac33bd3ed449e39300e7f2937f9e35c277916f2
anjumnr99 <anjumanowara99@gmail.com>
2023-11-26 07:29:53 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-
-require('dotenv').config()
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+
+}));
 
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('Value of token in the middleware', token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" })
+  }
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" })
+    }
+    // if token is valid it would be in the decode
+    console.log('Value in the token is : ', decode);
+    req.userInfo = decode;
 
+    next();
+  })
+}
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7nwjyzo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,9 +49,45 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+     const packageCollection = client.db("travelTourDB").collection("packages");
+
+
+     
+    // auth related API
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res       // setting the token in cookie
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({ success: true })
+    })
+
+   
+
+    app.get('/packages', async (req, res) => {
+      const result = await packageCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -45,9 +102,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Travel Tour is running!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Travel Tour is running on port ${port}`)
-  })
+  res.send('Travel Tour is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Travel Tour is running on port ${port}`)
+})
